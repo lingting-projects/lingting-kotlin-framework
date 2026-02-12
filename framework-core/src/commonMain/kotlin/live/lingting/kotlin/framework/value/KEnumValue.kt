1515f6a868368a5050a5ad4bf385fee07a27327d
lingting <sunlisten.gzm@foxmail.com>
2026-02-12 14:39:39 +0800

    :sparkles: 添加枚举序列化

---
+++ framework-core/src/commonMain/kotlin/live/lingting/kotlin/framework/value/KEnumValue.kt
@@ -0,0 +1,10 @@
+package live.lingting.kotlin.framework.value
+
+/**
+ * @author lingting 2026/2/12 11:43
+ */
+interface KEnumValue<V> {
+
+    val value: V
+
+}
---
+++ framework-core/src/jvmMain/kotlin/live/lingting/kotlin/framework/util/EnumUtils.jvm.kt
@@ -0,0 +1,5 @@
+package live.lingting.kotlin.framework.util
+
+internal actual fun <E : Enum<E>> EnumUtils.internalGetValue(e: Enum<E>): Any? {
+    return null
+}
