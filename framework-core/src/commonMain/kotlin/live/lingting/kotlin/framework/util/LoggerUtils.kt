06a3a0291070c8fb856372bb30249405c6ee0ec1
lingting <sunlisten.gzm@foxmail.com>
2026-02-13 17:20:45 +0800

    :sparkles: LoggerUtils.kt 支持使用实例获取

--- framework-core/src/commonMain/kotlin/live/lingting/kotlin/framework/util/LoggerUtils.kt
+++ framework-core/src/commonMain/kotlin/live/lingting/kotlin/framework/util/LoggerUtils.kt
@@ -28,6 +28,11 @@ object LoggerUtils {
         return KotlinLogging.logger(name)
     }
 
+    @JvmStatic
+    fun <T : Any> logger(t: T): Logger {
+        return logger(t::class)
+    }
+
     inline val InlineLogger.log get() = logger()
 
     interface InlineLogger
