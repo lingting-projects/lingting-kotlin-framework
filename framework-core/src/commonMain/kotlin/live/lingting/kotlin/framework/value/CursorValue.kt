46f7bfbf922c8de4705cb9568af17c2f64981f6c
lingting <sunlisten.gzm@foxmail.com>
2026-03-02 17:38:47 +0800

    :sparkles: 添加游标值

---
+++ framework-core/src/commonMain/kotlin/live/lingting/kotlin/framework/value/CursorValue.kt
@@ -0,0 +1,36 @@
+package live.lingting.kotlin.framework.value
+
+import kotlinx.coroutines.flow.asFlow
+
+/**
+ * @author lingting 2026/2/26 17:31
+ */
+abstract class CursorValue<T> : Iterator<T> {
+
+    protected abstract fun nextBatchData(): List<T>?
+
+    protected val source = sequence {
+        while (true) {
+            val batch = nextBatchData()
+            if (batch.isNullOrEmpty()) break
+            yieldAll(batch)
+        }
+    }
+
+    protected val iterator = source.iterator()
+
+    override fun hasNext(): Boolean {
+        return iterator.hasNext()
+    }
+
+    override fun next(): T {
+        return iterator.next()
+    }
+
+    fun asSequence(): Sequence<T> = source
+
+    fun asFlow() = source.asFlow()
+
+    fun toList(): List<T> = source.toList()
+
+}
