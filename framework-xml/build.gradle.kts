47bb5edf7ff7481545ea6c92051e8cf4e0ff0537
lingting <sunlisten.gzm@foxmail.com>
2026-03-03 17:02:58 +0800

    :sparkles: xml扩展

---
+++ framework-xml/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("lingting_kmp")
+    id("lingting_publish")
+    alias(libs.plugins.kotlin.serialization)
+}
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                api(project(":framework-core"))
+                api(libs.kotlin.serialization.xml)
+            }
+        }
+    }
+}
