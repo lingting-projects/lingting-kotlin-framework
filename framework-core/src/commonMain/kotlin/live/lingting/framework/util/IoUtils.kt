4af2bde86c0b4a99f57888d6c5f48ffc38f865d0
lingting <sunlisten.gzm@foxmail.com>
2026-03-12 15:42:25 +0800

    :sparkles: IoUtils支持source转buffer

--- framework-core/src/commonMain/kotlin/live/lingting/framework/util/IoUtils.kt
+++ framework-core/src/commonMain/kotlin/live/lingting/framework/util/IoUtils.kt
@@ -1,6 +1,9 @@
 package live.lingting.framework.util
 
+import kotlinx.io.Buffer
+import kotlinx.io.RawSource
 import live.lingting.framework.io.ByteArraySource
+import live.lingting.framework.util.DataSizeUtils.mb
 import kotlin.jvm.JvmOverloads
 import kotlin.jvm.JvmStatic
 
@@ -12,5 +15,19 @@ object IoUtils {
     @JvmStatic
     @JvmOverloads
     fun ByteArray.source(offset: Int = 0, length: Int = -1) = ByteArraySource(this, offset, length)
+
+    /**
+     * 不要用于转换网络流!
+     */
+    @JvmStatic
+    @JvmOverloads
+    fun RawSource.buffer(byteCount: Long = 1.mb.bytes): Buffer {
+        val buffer = Buffer()
+        while (true) {
+            val len = readAtMostTo(buffer, byteCount)
+            if (len < 0L) break
+        }
+        return buffer
+    }
 
 }
